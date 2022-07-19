f4fd4a84b586bbd5866fee43fba62334f12818d6
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-07-18 23:53:28 -0400

    split code reduced, more efficient

--- target_labels_train_split.py
+++ target_labels_train_split.py
@@ -6,9 +6,10 @@
 
 """Get dataset train/val/test split"""
 
-import argparse
-import os
+
+import csv
 import pickle
+from webbrowser import Chrome
 
 import numpy as np
 import pandas as pd
@@ -22,14 +23,33 @@ from utils import time_decorator, TISSUE_TPM_KEYS
 
 
 @time_decorator(print_args=True)
+
+def chr_split_train_test_val(gff, test_chrs, val_chrs):
+    """
+    create a list of training, test, and val IDs
+    """
+    with open(gff, newline = '') as file:
+        genes = {line[3]: line[0] for line in csv.reader(file, delimiter='\t')}
+
+    return {
+        'train': [gene for gene in genes if genes[gene] not in test_chrs + val_chrs],
+        'test': [gene for gene in genes if genes[gene] in test_chrs],
+        'validation': [gene for gene in genes if genes[gene] in val_chrs],
+    }
+
 def std_dev_and_mean_gtex() -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Get means and standard deviation for TPM across all GTEx tissues"""
     
-
 def main() -> None:
     """Pipeline to generate dataset split and target values"""
-    gct = ''
 
+    gff = '/ocean/projects/bio210019p/stevesho/data/preprocess/shared_data/interaction/gencode_v26_genes_only_with_GTEx_targets.bed'
+    
+    test = chr_split_train_test_val(
+        gff=gff,
+        test_chrs=['chr8', 'chr9'],
+        val_chrs=['chr7', 'chr13'],
+    )
 
 if __name__ == '__main__':
     main()
