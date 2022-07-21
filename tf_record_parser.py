d755fe176c03d403c2463d9dc3820747f61e8b78
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-07-20 21:31:08 -0400

    issue with filename double

--- tf_record_parser.py
+++ tf_record_parser.py
@@ -138,7 +138,7 @@ class GGraphMutagenesisTFRecordProcessor:
     def _open_graph(self, gene):
         '''utility to open the graph file'''
         tissue = gene.split('_')[1]
-        with open(f'{tissue}/parsing/graphs/{gene}_{tissue}', 'rb') as f:
+        with open(f'{tissue}/parsing/graphs/{gene}', 'rb') as f:
             return pickle.load(f)
 
     def create_tfrecords(self, mode="train"):
