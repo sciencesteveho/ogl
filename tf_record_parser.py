65aff6a26864a81760f7df19d1810a065eb4097d
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-07-29 01:36:15 -0400

    changing max_nodes

--- tf_record_parser.py
+++ tf_record_parser.py
@@ -130,7 +130,7 @@ class GGraphMutagenesisTFRecordProcessor:
         self.name = name
         self.feature_dim = feature_dim
         self.shuffle_raw_train_data = params.get("shuffle_raw_train_data", True)
-        self.max_num_nodes = params.get("max_num_nodes", 19987)
+        self.max_num_nodes = params.get("max_num_nodes", 9999)
         self.task_type = params.get("task_type", "binary_classification")
         self.normalize = params.get("normalize", True)
 
