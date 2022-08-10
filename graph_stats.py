89ea20d37a1f5f08ffa1a8b94c13c430272a74e7
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-08-10 17:31:06 -0400

    lots of changes, but getting sparse stuff

--- graph_stats.py
+++ graph_stats.py
@@ -8,6 +8,8 @@
 
 import argparse
 import pickle
+import numpy as np
+import scipy.sparse as sp
 
 from utils import genes_from_gff, time_decorator
 
@@ -27,6 +29,7 @@ def _open_and_add_tissue(filename, tissue):
         f'{gene}_{tissue}':value for gene, value in stats.items()
     }
 
+
 def _cat_stat_dicts(tissue_params):
     for idx, tup in enumerate(tissue_params):
         if idx == 0:
@@ -35,6 +38,7 @@ def _cat_stat_dicts(tissue_params):
             update = _open_and_add_tissue(tup[0], tup[1])
             shared.update(update)
     return shared
+
 
 def _edge_and_nodes_per_gene(
     gff,
@@ -47,48 +51,86 @@ def _edge_and_nodes_per_gene(
         for _, gene in enumerate(genes)
     }
 
+
 def _summed_counts(graph_stats):
-    return sum([graph_stats[gene][0] for gene in graph_stats.keys()]), sum([graph_stats[gene][1] for gene in graph_stats.keys()])
+    return (
+        sum([graph_stats[gene][0] for gene in graph_stats.keys()]),
+        sum([graph_stats[gene][1] for gene in graph_stats.keys()])
+    )
+
+
+def _graph_sparsity(filename):
+    with open(filename, 'rb') as file:
+        graph = pickle.load(file)
+    num_nodes = graph["num_nodes"]
+    row, col = graph["edge_index"].numpy()
+    adj = sp.coo_matrix(
+        (np.ones_like(row),
+        (row, col)),
+        shape=(num_nodes, num_nodes)
+        ).toarray()
+    zeros = np.count_nonzero(adj)
+    nonzero = adj.size - zeros
+    sparse_percentage = zeros/adj.size
+    return (adj.size, nonzero, "{:.1%}".format(sparse_percentage))
 
 
 def main() -> None:
     """Save num_nodes as array for each individual tissue"""
+    # ctcf = 'ENSG00000102974.14'
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-t', '--tissue', type=str, required=True)
+    # args = parser.parse_args()
+
+    # graph_stats = _edge_and_nodes_per_gene(
+    #     gff = '/ocean/projects/bio210019p/stevesho/data/preprocess/shared_data/interaction/gencode_v26_genes_only_with_GTEx_targets.bed',
+    #     directory = f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs',
+    #     tissue=args.tissue
+    #     )
+
+    # ### save
+    # node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/count_num_nodes'
+    # with open(f'{node_dir}/num_nodes_{args.tissue}.pkl', 'wb') as output:
+    #     pickle.dump(graph_stats, output)
+
+    # tissue_params = [
+    #     ('num_nodes_hippocampus.pkl', 'hippocampus'),
+    #     ('num_nodes_left_ventricle.pkl', 'left_ventricle'),
+    #     ('num_nodes_mammary.pkl', 'mammary'),   
+    #     ]
+
+    # ### unfiltered
+    # all_stats_dict = _cat_stat_dicts(tissue_params)
+    # total_nodes, total_edges = _summed_counts(all_stats_dict)
+
+    # with open('targets_filtered.pkl', 'rb') as f:
+    #     filtered_targets = pickle.load(f)
+
+    # filtered_genes = list(filtered_targets['train'].keys()) + list(filtered_targets['test'].keys()) + list(filtered_targets['validation'].keys())
+
+    # filtered_stats = {
+    #     gene: value for gene, value
+    #     in all_stats_dict.items()
+    #     if gene in filtered_genes
+    # }
+    # total_nodes, total_edges = _summed_counts(filtered_stats)
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tissue', type=str, required=True)
+    parser.add_argument('-p', '--partition', type=str, required=True)
     args = parser.parse_args()
 
-    graph_stats = _edge_and_nodes_per_gene(
-        gff = '/ocean/projects/bio210019p/stevesho/data/preprocess/shared_data/interaction/gencode_v26_genes_only_with_GTEx_targets.bed',
-        directory = f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs',
-        tissue=args.tissue
-        )
+    savedir = '/ocean/projects/bio210019p/stevesho/data/preprocess/sparse_check'
+    directory = f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs'
+    gff = '/ocean/projects/bio210019p/stevesho/data/preprocess/shared_data/interaction/gencode_v26_genes_only_with_GTEx_targets.bed'
 
-    ### save
-    node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/count_num_nodes'
-    with open(f'{node_dir}/num_nodes_{args.tissue}.pkl', 'wb') as output:
-        pickle.dump(graph_stats, output)
+    genes = list(genes_from_gff(gff))
+    sparse = []
+    for gene in genes:
+        sparse.append(f'{directory}/gene_{args.tissue}')
 
-    tissue_params = [
-        ('num_nodes_hippocampus.pkl', 'hippocampus'),
-        ('num_nodes_left_ventricle.pkl', 'left_ventricle'),
-        ('num_nodes_mammary.pkl', 'mammary'),   
-        ]
-
-    ### unfiltered
-    all_stats_dict = _cat_stat_dicts(tissue_params)
-    total_nodes, total_edges = _summed_counts(all_stats_dict)
-
-    with open('targets_filtered.pkl', 'rb') as f:
-        filtered_targets = pickle.load(f)
-
-    filtered_genes = list(filtered_targets['train'].keys()) + list(filtered_targets['test'].keys()) + list(filtered_targets['validation'].keys())
-
-    filtered_stats = {
-        gene: value for gene, value
-        in all_stats_dict.items()
-        if gene in filtered_genes
-    }
-    total_nodes, total_edges = _summed_counts(filtered_stats)
+    with open(f'{savedir}/sparse_vals_{args.tissue}.pkl', 'wb') as output:
+        pickle.dump(sparse, output)
+    
     
 if __name__ == '__main__':
     main()
--- plot_figures.py
+++ plot_figures.py
@@ -3,10 +3,18 @@
 
 """Code to plot various data"""
 
+import csv
 import pickle
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from collections import Counter
+
+
+def _set_matplotlib_publication_parameters():
+    plt.rcParams.update({'font.size': 7})  # set font size
+    plt.rcParams["font.family"] = 'Helvetica'  # set font
 
 
 def _cat_numnodes(files):
@@ -19,27 +27,49 @@ def _cat_numnodes(files):
     return num_nodes
 
 
+def feat_list(file, col_idx):
+    with open(file, newline='') as file:
+        return [
+            line[col_idx]
+            for line in csv.reader(file, delimiter='\t')
+        ]
+
+
+def uniq_feats(feat_list):
+    return Counter(feat_list)
+
+
+def plot_num_nodes_dist(all_nodes, fontsize, kde_option=False):
+    sns.distplot(all_nodes, kde=kde_option)
+    plt.title('Distribution of num_nodes', fontsize=fontsize)
+    plt.xlabel('Number of graphs', fontsize=fontsize)
+    plt.ylabel('Number of nodes', fontsize=fontsize)
+    return plt
+
+
+def barplot_feats(uniq_feats):
+    '''Allo'''
+
+
 if __name__ == '__main__':
     ### set matplotlib parameters
-    plt.rcParams.update({'font.size': 7})  # set font size
-    plt.rcParams["font.family"] = 'Helvetica'  # set font
-    # plt.rcParams["figure.figsize"] = [34,18]
+    _set_matplotlib_publication_parameters()
 
     ### get num_nodes across all graphs
     files = ['num_nodes_hippocampus.pkl', 'num_nodes_left_ventricle.pkl', 'num_nodes_mammary.pkl']
     all_nodes = _cat_numnodes(files)
 
     ### plot with smoothed function
-    sns.distplot(all_nodes)
-    plt.title('Distribution of num_nodes', fontsize=7)
-    plt.xlabel('Number of graphs', fontsize=7)
-    plt.ylabel('Number of nodes', fontsize=7)
+    smoothed_plt = plot_num_nodes_dist(all_nodes, 7, kde_option=True)
 
     ### plot with absolute values
-    sns.distplot(all_nodes, kde=False)
-    plt.title('Distribution of num_nodes', fontsize=7)
-    plt.xlabel('Number of graphs', fontsize=7)
-    plt.ylabel('Number of nodes', fontsize=7)
+    absolute_plt = plot_num_nodes_dist(all_nodes, 7, kde_option=False)
+
+    ### uniq_feats for chromHMM
+    feats = feat_list('chromhmm_mammary.bed', 3)
+    uniq = uniq_feats(feats)
+    sns.barplot(x=list(uniq.keys()), y=list(uniq.values()), order=list(uniq.values()).sort())
+
 
 
 idx=0
