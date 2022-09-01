42b47544d5461775c7c22184d21848f1adbb1fea
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-09-01 17:23:36 -0400

    literal comma

--- prepare_bedfiles.py
+++ prepare_bedfiles.py
@@ -307,7 +307,7 @@ class GenomeDataPreprocessor:
                     pass
 
         ### Make symlinks and rename files that do not need preprocessing
-        nochange = ['ctcf' 'dnase', 'H3K27ac', 'H3K27me3', 'H3K36me3', 'H3K4me1', 'H3K4me3', 'H3K9me3', 'polr2a']
+        nochange = ['ctcf', 'dnase', 'H3K27ac', 'H3K27me3', 'H3K36me3', 'H3K4me1', 'H3K4me3', 'H3K9me3', 'polr2a']
         for datatype in nochange:
             if self.tissue_specific[datatype]:
                 src = f'{self.data_dir}/{self.tissue_specific[datatype]}'
