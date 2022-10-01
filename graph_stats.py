735f852eef0331c9f315109ef3a003421318a70e
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-10-01 19:19:44 -0400

    adjust record parser to parse a single file at a time

--- graph_stats.py
+++ graph_stats.py
@@ -1,3 +1,5 @@
+
+
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
@@ -92,53 +94,53 @@ def _percentage_of_zeroes(tup_list):
 
 def main() -> None:
     # """Save num_nodes as array for each individual tissue"""
-    # ctcf = 'ENSG00000102974.14'
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-t', '--tissue', type=str, required=True)
-    # args = parser.parse_args()
+    ctcf = 'ENSG00000102974.14'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--tissue', type=str, required=True)
+    args = parser.parse_args()
 
-    # root_dir='/ocean/projects/bio210019p/stevesho/data/preprocess'
-    # node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/check_num_nodes'
-
-    # genes = filtered_genes(f'{root_dir}/{args.tissue}/gene_regions_tpm_filtered.bed')
-
-    # graph_stats = _edge_and_nodes_per_gene(
-    #     genes=genes,
-    #     directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs',
-    #     tissue=args.tissue
-    #     )
-
-    # ### save
-    # with open(f'{node_dir}/num_nodes_{args.tissue}.pkl', 'wb') as output:
-    #     pickle.dump(graph_stats, output)
-
-
+    root_dir='/ocean/projects/bio210019p/stevesho/data/preprocess'
     node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/check_num_nodes'
-    tissue_params = [
-        ['num_nodes_hippocampus.pkl', 'hippocampus'],
-        ['num_nodes_left_ventricle.pkl', 'left_ventricle'],
-        ['num_nodes_mammary.pkl', 'mammary'],
-        ['num_nodes_liver.pkl', 'liver'],
-        ['num_nodes_lung.pkl', 'lung'],
-        ['num_nodes_pancreas.pkl', 'pancreas'],
-        ['num_nodes_skeletal_muscle.pkl', 'skeletal_muscle'],
-        ]
 
-    tissue_params = [[node_dir + '/' + x[0], x[1]] for x in tissue_params]
+    genes = filtered_genes(f'{root_dir}/{args.tissue}/gene_regions_tpm_filtered.bed')
 
-    ### unfiltered
-    all_stats_dict = _cat_stat_dicts(tissue_params)
-    # total_nodes, total_edges = _summed_counts(all_stats_dict)
+    graph_stats = _edge_and_nodes_per_gene(
+        genes=genes,
+        directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs',
+        tissue=args.tissue
+        )
 
-    node_counts = [x[0] for x in all_stats_dict.values()]
-    edge_counts = [x[1] for x in all_stats_dict.values()]
+    ### save
+    with open(f'{node_dir}/graph_stats_{args.tissue}.pkl', 'wb') as output:
+        pickle.dump(graph_stats, output)
 
-    ### save 
-    with open(f'{node_dir}/node_count.pkl', 'wb') as output:
-        pickle.dump(node_counts, output)
 
-    with open(f'{node_dir}/edge_count.pkl', 'wb') as output:
-        pickle.dump(edge_counts, output)
+    # node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/check_num_nodes'
+    # tissue_params = [
+    #     ['num_nodes_hippocampus.pkl', 'hippocampus'],
+    #     ['num_nodes_left_ventricle.pkl', 'left_ventricle'],
+    #     ['num_nodes_mammary.pkl', 'mammary'],
+    #     ['num_nodes_liver.pkl', 'liver'],
+    #     ['num_nodes_lung.pkl', 'lung'],
+    #     ['num_nodes_pancreas.pkl', 'pancreas'],
+    #     ['num_nodes_skeletal_muscle.pkl', 'skeletal_muscle'],
+    #     ]
+
+    # tissue_params = [[node_dir + '/' + x[0], x[1]] for x in tissue_params]
+
+    # ### unfiltered
+    # all_stats_dict = _cat_stat_dicts(tissue_params)
+    # # total_nodes, total_edges = _summed_counts(all_stats_dict)
+
+    # node_counts = [x[0] for x in all_stats_dict.values()]
+    # edge_counts = [x[1] for x in all_stats_dict.values()]
+
+    # ### save 
+    # with open(f'{node_dir}/node_count.pkl', 'wb') as output:
+    #     pickle.dump(node_counts, output)
+
+    # with open(f'{node_dir}/edge_count.pkl', 'wb') as output:
+    #     pickle.dump(edge_counts, output)
 
     # parser = argparse.ArgumentParser()
     # parser.add_argument('-t', '--tissue', type=str, required=True)
