f705d2e8fbecabfd1d860faf9ec17ecf4cb364fe
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-10-01 23:46:45 -0400

    safety

--- local_context_parser.py
+++ local_context_parser.py
@@ -610,41 +610,41 @@ class LocalContextFeatures:
                         )
                 outfile.close()
 
-        # # process windows and renaming 
-        # pool = Pool(processes=32)
-        # bedinstance = pool.map(self._region_specific_features_dict,\
-        #     [bed for bed in self.bedfiles])
-        # pool.close()  # re-open and close pool after every multi-process
+        # process windows and renaming 
+        pool = Pool(processes=32)
+        bedinstance = pool.map(self._region_specific_features_dict,\
+            [bed for bed in self.bedfiles])
+        pool.close()  # re-open and close pool after every multi-process
 
-        # # convert back to dictionary
-        # bedinstance = {key.casefold():value for element in bedinstance for key, value in element.items()}
+        # convert back to dictionary
+        bedinstance = {key.casefold():value for element in bedinstance for key, value in element.items()}
 
-        # # sort and extend windows according to FEAT_WINDOWS
-        # bedinstance_sorted, bedinstance_slopped = self._slop_sort(bedinstance=bedinstance, chromfile=self.chromfile)
+        # sort and extend windows according to FEAT_WINDOWS
+        bedinstance_sorted, bedinstance_slopped = self._slop_sort(bedinstance=bedinstance, chromfile=self.chromfile)
 
-        # # save a list of the nodes and their indexes
-        # self._save_feature_indexes(bedinstance_sorted)
+        # save a list of the nodes and their indexes
+        self._save_feature_indexes(bedinstance_sorted)
 
-        # # save intermediate files
-        # _save_intermediate(bedinstance_sorted, folder='sorted')
-        # _save_intermediate(bedinstance_slopped, folder='slopped')
+        # save intermediate files
+        _save_intermediate(bedinstance_sorted, folder='sorted')
+        _save_intermediate(bedinstance_slopped, folder='slopped')
 
-        # # pre-concatenate to save time
-        # all_files = f'{self.parse_dir}/intermediate/sorted/all_files_concatenated.bed'
-        # _pre_concatenate_all_files(all_files)
+        # pre-concatenate to save time
+        all_files = f'{self.parse_dir}/intermediate/sorted/all_files_concatenated.bed'
+        _pre_concatenate_all_files(all_files)
 
-        # # perform intersects across all feature types - one process per nodetype
-        # pool = Pool(processes=self.NODE_CORES)
-        # pool.starmap(self._bed_intersect, zip(self.NODES, repeat(all_files)))
-        # pool.close()
+        # perform intersects across all feature types - one process per nodetype
+        pool = Pool(processes=self.NODE_CORES)
+        pool.starmap(self._bed_intersect, zip(self.NODES, repeat(all_files)))
+        pool.close()
 
-        # # get size and all attributes - one process per nodetype
-        # pool = Pool(processes=self.NODE_CORES)
-        # pool.map(self._aggregate_attributes, self.NODES)
-        # pool.close()
+        # get size and all attributes - one process per nodetype
+        pool = Pool(processes=self.NODE_CORES)
+        pool.map(self._aggregate_attributes, self.NODES)
+        pool.close()
 
-        # # parse edges into individual files
-        # self._generate_edges()
+        # parse edges into individual files
+        self._generate_edges()
 
         # save node attributes as reference for later - one process per nodetype
         pool = Pool(processes=self.NODE_CORES)
