34939a9abcad4bf14ae6d34676464e1f6aee5f51
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-10-02 23:11:35 -0400

    arg typo

--- graph_stats.py
+++ graph_stats.py
@@ -134,7 +134,7 @@ def main() -> None:
 
     scaler = MinMaxScaler()
     for tissue in TISSUES:
-        directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs'
+        directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{tissue}/parsing/graphs'
         genes = filtered_genes(f'{root_dir}/{tissue}/gene_regions_tpm_filtered.bed')
         genes = [gene for gene in genes if gene in train]
         for gene in genes:
