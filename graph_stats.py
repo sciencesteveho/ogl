e83131c2cb337005b007504079d9d02fd1c9e6f7
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2022-10-02 23:04:43 -0400

    using graph stats as minmax scaler

--- graph_stats.py
+++ graph_stats.py
@@ -9,6 +9,7 @@
 """Store num_nodes in array"""
 
 import argparse
+import joblib
 import pickle
 import os
 
@@ -16,8 +17,21 @@ import numpy as np
 import scipy.sparse as sp
 
 import pybedtools
+from sklearn.preprocessing import MinMaxScaler
 
+from target_labels_train_split import _chr_split_train_test_val
 from utils import genes_from_gff, filtered_genes, time_decorator
+
+
+TISSUES = [
+    'hippocampus',
+    'liver',
+    'lung',
+    'mammary', 
+    'pancreas',
+    'skeletal_muscle',
+    'left_ventricle',
+    ]
 
 
 @time_decorator(print_args=True)
@@ -97,24 +111,49 @@ def _percentage_of_zeroes(tup_list):
 def main() -> None:
     # """Save num_nodes as array for each individual tissue"""
     # ctcf = 'ENSG00000102974.14'
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--tissue', type=str, required=True)
+    parser.add_argument('-f', '--feat', type=int, required=True)
     args = parser.parse_args()
 
-    root_dir='/ocean/projects/bio210019p/stevesho/data/preprocess'
-    node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/check_num_nodes'
+    genes = genes_from_gff('/ocean/projects/bio210019p/stevesho/data/preprocess/shared_data/interaction/gencode_v26_genes_only_with_GTEx_targets.bed')
+    test_chrs = ['chr8', 'chr9']
+    val_chrs = ['chr7', 'chr13']
 
-    genes = filtered_genes(f'{root_dir}/{args.tissue}/gene_regions_tpm_filtered.bed')
-
-    graph_stats = _edge_and_nodes_per_gene(
+    # split genes by chr holdouts
+    split = _chr_split_train_test_val(
         genes=genes,
-        directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs',
-        tissue=args.tissue
-        )
+        test_chrs=test_chrs,
+        val_chrs=val_chrs,
+    )
+
+    train = split['train']
+    root_dir='/ocean/projects/bio210019p/stevesho/data/preprocess'
+    node_dir='/ocean/projects/bio210019p/stevesho/data/preprocess/check_num_nodes'
+    output_dir='/ocean/projects/bio210019p/stevesho/data/preprocess/data_scaler'
+
+    scaler = MinMaxScaler()
+    for tissue in TISSUES:
+        directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs'
+        genes = filtered_genes(f'{root_dir}/{args.tissue}/gene_regions_tpm_filtered.bed')
+        genes = [gene for gene in genes if gene in train]
+        for gene in genes:
+            with open(f'{directory}/{gene}_{tissue}', 'rb') as f:
+                g = pickle.load(f)
+            node_feat = g['node_feat']
+            scaler.partial_fit(node_feat[:,args.feat].reshape(-1, 1))
 
     ### save
-    with open(f'{node_dir}/feat_stats_{args.tissue}.pkl', 'wb') as output:
-        pickle.dump(graph_stats, output)
+    joblib.dump(scaler, f'{output_dir}/feat_{args.feat}_scaler.pt')
+
+    # graph_stats = _edge_and_nodes_per_gene(
+    #     genes=genes,
+    #     directory=f'/ocean/projects/bio210019p/stevesho/data/preprocess/{args.tissue}/parsing/graphs',
+    #     tissue=args.tissue
+    #     )
+
+    # with open(f'{node_dir}/feat_stats_{args.tissue}.pkl', 'wb') as output:
+    #     pickle.dump(graph_stats, output)
 
 
     # node_dir = '/ocean/projects/bio210019p/stevesho/data/preprocess/check_num_nodes'
