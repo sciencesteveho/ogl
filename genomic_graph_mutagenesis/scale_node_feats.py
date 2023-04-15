da3838e98c698afef283e4f6735362c70eb428a7
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2023-04-15 18:59:59 -0400

    fix files in scaler

--- genomic_graph_mutagenesis/scale_node_feats.py
+++ genomic_graph_mutagenesis/scale_node_feats.py
@@ -29,11 +29,11 @@ if __name__ == "__main__":
 
     scalers = {i: joblib.load(f'{scale_dir}/feat_{i}_scaler.pt') for i in range(0, 36)}
 
-    with open(f'{graph_dir}/{gene}_{args.tissue}', 'rb') as f:
+    with open(f'{graph_dir}/{args.tissue}_full_graph.pkl', 'rb') as f:
         g = pickle.load(f)
-    node_feat = g['node_feat'].astype(np.float32)
+    node_feat = g['node_feat']
     for i in range(0, 36):
         node_feat[:,i] = scalers[i].transform(node_feat[:,i].reshape(-1,1)).reshape(1, -1)[0]
     g['node_feat'] = node_feat 
-    with open(f'{out_dir}/{gene}_{args.tissue}', 'wb') as output:
+    with open(f'{out_dir}/graph.pkl', 'wb') as output:
         pickle.dump(g, output)
