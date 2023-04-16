fix missing arg in filename for scaling node feats

genomic_graph_mutagenesis/graph_to_pytorch.py
@@ -25,7 +25,17 @@ from torch_geometric.data import Data, InMemoryDataset
 
 from utils import parse_yaml
 
-# Generate a class to load graph data from np arrays to a pytorch geometric Data object
+# Generate a function to create a training mask for the graph based on a nested dictionary with the keys "train", "test", and "validation"
+def create_mask(
+    graph_indexes: str,
+    ) -> np.ndarray:
+    """Create mask for graph"""
+    # load graph indexes
+    with open(graph_indexes, 'rb') as f:
+        graph_index = pickle.load(f)
+    
+    
+    
 
 
 def _combined_graph_arrays(