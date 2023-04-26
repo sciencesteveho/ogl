e0faa275469c7eb90341779a8a558b45c27f0de8
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2023-04-26 13:52:50 -0400

    remove anchor by loops

--- genomic_graph_mutagenesis/base_graph_loops.py
+++ genomic_graph_mutagenesis/base_graph_loops.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# // TO-DO //
-# - [ ] first TODO
-#   - [ ] nested TODO
+# / TODO /
+# - [ ] properly instatiate the tss, and remove the hardcode name for tss or enhancers and use feats
+# - [ ] get active enhancers from FENRIR
+# - [ ] only keep TSS with gene targets
+# - [ ] adjust the edges output to resembl that from edge_parser.py
+# - [ ] figure out why product returns key error. need to check for empty keys
 
 
 """Create a base graph from chromatin loop data.
@@ -11,15 +14,32 @@
 We filter for distal ELSs and link them to other enhancers and promoters (loop
 overlap) or genes (within 2kb of a loop anchor)."""
 
+import itertools
 from typing import Dict, List, Tuple
 
 import pybedtools
 
 
+def _load_tss(tss_path: str) -> pybedtools.BedTool:
+    """Load TSS file and ignore any TSS that do not have a gene target. 
+    Returns:
+        pybedtools.BedTool - TSS w/ target genes
+    """
+    tss = pybedtools.BedTool(tss_path)
+    return tss.filter(lambda x: x[3].split('_')[3] != '').saveas()
+
+
 def _split_chromatin_loops(
     loop_path: str,
 ) -> Tuple[pybedtools.BedTool, pybedtools.BedTool]:
-    """_summary_ of function"""
+    """_summary_
+
+    Args:
+        loop_path (str): _description_
+
+    Returns:
+        Tuple[pybedtools.BedTool, pybedtools.BedTool]: _description_
+    """
     first_anchor = pybedtools.BedTool(loop_path)
     second_anchor = pybedtools.BedTool(
         "\n".join(
@@ -42,28 +62,50 @@ def _loop_within_distance(
     tss: pybedtools.BedTool,
     distance: int,
 ) -> pybedtools.BedTool:
-    """_summary_ of function"""
+    """_summary_
+
+    Args:
+        loops (pybedtools.BedTool): _description_
+        tss (pybedtools.BedTool): _description_
+        distance (int): _description_
+
+    Returns:
+        pybedtools.BedTool: _description_
+    """
     return loops.window(tss, w=distance)
 
 
-def _flatten_anchors(*beds) -> Dict[str, List[str]]:
-    """Creates"""
+def _flatten_anchors(*beds: pybedtools.BedTool) -> Dict[str, List[str]]:
+    """Creates a dict to store each anchor and its overlaps. Adds the feature by
+    ignoring the first 7 columns of the bed file and adding whatever is left."""
     anchor = {}
     for bed in beds:
         for feature in bed:
-            anchor.setdefault("_".join(feature[0:3]), []).append(
-                "_".join(feature[7:13])
-            )
+            anchor.setdefault("_".join(feature[0:3]), []).append("_".join(feature[7:]))
     return anchor
 
 
-def _loop_connections_overlap(bed_one, bed_two) -> None:
+def _loop_edges(
+    loops: pybedtools.BedTool,
+    first_anchor_edges: Dict[str, List[str]],
+    second_anchor_edges: Dict[str, List[str]],
+) -> None:
     """Return a list of edges that are connected by their overlap over chromatin
-    loop anchors
-
-    First we flatten the anchors to keep all of their overlaps in a list. Then,
-    we create tuples from one feature to another based on the corresponding
-    anchor point list."""
+    loop anchors by matching the anchor names across dicts"""
+    edges = []
+    for loop in loops:
+        first_anchor = "_".join(loop[0:3])
+        second_anchor = "_".join(loop[3:6])
+        try:
+            uniq_edges = list(
+                itertools.product(
+                    first_anchor_edges[first_anchor], second_anchor_edges[second_anchor]
+                )
+            )
+            edges.extend(uniq_edges)
+        except KeyError:
+            continue
+    return edges
 
 
 def main() -> None:
@@ -75,13 +117,20 @@ def main() -> None:
     tss_path = "/ocean/projects/bio210019p/stevesho/data/bedfile_preparse/reftss/reftss_annotated.bed"
 
     enhancers = pybedtools.BedTool(enhancer_path)
+    tss = _load_tss(tss_path)
     first_anchor, second_anchor = _split_chromatin_loops(loop_path)
 
     anchor_one_overlaps = _loop_direct_overlap(first_anchor, enhancers)
     anchor_two_overlaps = _loop_direct_overlap(second_anchor, enhancers)
-    anchor_one_tss = _loop_within_distance(first_anchor, tss_path, 2000)
-    anchor_two_tss = _loop_within_distance(second_anchor, tss_path, 2000)
+    anchor_one_tss = _loop_within_distance(first_anchor, tss, 2000)
+    anchor_two_tss = _loop_within_distance(second_anchor, tss, 2000)
 
+    first_anchor_edges = _flatten_anchors(anchor_one_overlaps, anchor_one_tss)
+    second_anchor_edges = _flatten_anchors(anchor_two_overlaps, anchor_two_tss)
+
+    edges = _loop_edges(
+        first_anchor, first_anchor_edges, second_anchor_edges
+    )
 
 if __name__ == "__main__":
     main()
