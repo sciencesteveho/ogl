a78320f05585fa74efdaad9b5f881cab38e2d6ce
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2023-07-11 17:51:18 -0400

    fix config keys

--- genomic_graph_mutagenesis/dataset_split.py
+++ genomic_graph_mutagenesis/dataset_split.py
@@ -25,8 +25,28 @@ from utils import TISSUES
 DATA_SPLITS = ["train", "test", "validation"]
 
 
+def _tpm_all_tissue_median(gct_file: str) -> None:
+    """Get the median TPM per gene across ALL samples within GTEx V8 GCT and
+    saves it. Because the file is large and requires a lot of memory, we ran
+    this separately from the main function.
+
+    Args:
+        gct_file (str): /path/to/gtex gct file
+    """
+    df = parse(gct_file).data_df
+    median_series = pd.Series(df.median(axis=1), name="all_tissues").to_frame()
+    median_series.to_pickle("gtex_tpm_median_across_all_tissues.pkl")
+
+
 def _genes_from_gff(gff: str) -> List[str]:
-    """Get list of gtex genes from GFF file"""
+    """Get list of gtex genes from GFF file
+
+    Args:
+        gff (str): /path/to/genes.gtf
+
+    Returns:
+        List[str]: genes
+    """
     with open(gff, newline="") as file:
         return {
             line[3]: line[0]
@@ -40,10 +60,22 @@ def _chr_split_train_test_val(
     genes,
     test_chrs,
     val_chrs,
-    tissue_append: bool = False,
-):
-    """
-    Create a list of training, split, and val IDs
+    tissue_append: bool = True,
+) -> Dict[str, List[str]]:
+    """Creates training, test, and validation splits for genes based on
+    chromosome. Adds tissues to each label to differentiate targets between
+    graphs
+
+    Args:
+        genes (_type_): list of all genes in genome.
+        test_chrs (_type_)
+        val_chrs (_type_)
+        tissue_append (bool, optional): Whether or not to append tissue name to
+        ID. Defaults to True.
+
+    Returns:
+        Dict[str, List[str]]: dictionary of genes split into train, test, and
+        validation.
     """
     if tissue_append:
         return {
@@ -76,18 +108,19 @@ def _chr_split_train_test_val(
         }
 
 
-def _tpm_all_tissue_median(gct_file):
-    """Get the median TPM per gene across ALL samples within GTEx V8 GCT"""
-    df = parse(gct_file).data_df
-    median_series = pd.Series(df.median(axis=1), name="all_tissues").to_frame()
-    median_series.to_pickle("gtex_tpm_median_across_all_tissues.pkl")
+def _get_protein_adbunance_median_across_all_tissues_samples(
+    protein_abundance_matrix: str,
+) -> pd.DataFrame:
+    """Returns a pandas object with median protein abundance for each gene
+    across all samples and tissues. Values are log2, so we inverse log them,
+    as we transform them ourselves later.
 
+    Args:
+        protein_abundance_matrix (str): /path/to/protein_abundance_matrix.csv
 
-def _protein_abundance_all_tissue_median(protein_abundance_matrix: str):
-    """
-    For now, "BRAIN-CORTEX" values are being used for hippocampus. We choose cortex due
-    to similarites shown between the tissues in GTEx Consortium, Science, 2020.
-    Values are log2 so we inverse log them (2^x)
+    Returns:
+        _type_: pandas series with median protein abundance for each gene across
+        all samples and tissues
     """
     df = pd.read_csv(protein_abundance_matrix, sep=",", index_col="gene.id.full").drop(
         columns=["gene.id"]
@@ -97,14 +130,23 @@ def _protein_abundance_all_tissue_median(protein_abundance_matrix: str):
     return pd.Series(df.median(axis=1), name="all_tissues").to_frame()
 
 
-def _protein_std_dev_and_mean(
+def _get_protein_adundance_tissue_matrix(
     protein_abundance_medians: str,
     graph: str = "tissue",
 ) -> pd.DataFrame:
-    """Get means and standard deviation for protein abundance for specific tissue
-    Calulates fold change of median of tissue relative to all_tissue_median
+    """Gets
+
+    Args:
+        protein_abundance_medians (str): _description_
+        graph (str, optional): _description_. Defaults to "tissue".
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        pd.DataFrame: _description_
     """
-    if graph == "tissues":
+    if graph == "tissue":
         tissues = [
             "Heart Ventricle",
             "Brain Cortex",
@@ -136,9 +178,9 @@ def _protein_std_dev_and_mean(
 
 @time_decorator(print_args=False)
 def _calculate_fold_change_from_medians(
-    median_across_tissues,
     median_matrix,
-    type=None,
+    median_across_tissues,
+    type: str = "tpm",
 ) -> pd.DataFrame:
     """_lorem"""
     if type == "tpm":
@@ -146,20 +188,18 @@ def _calculate_fold_change_from_medians(
     else:
         regex = ((" ", "_"), ("", ""))  # second added as placeholder so we can use *r
 
-    df = pd.concat([median_across_tissues, median_matrix], axis=1)
+    df = pd.concat([median_matrix, median_across_tissues], axis=1)
+    df["all_tissues"] = np.log2(df["all_tissues"] + 0.25)
     for tissue in list(df.columns)[:-1]:
         tissue_rename = tissue.casefold()
         for r in regex:
             tissue_rename = tissue_rename.replace(*r)
         df.rename(columns={f"{tissue}": f"{tissue_rename}"}, inplace=True)
-        df[f"{tissue_rename}"] = np.log2(df[f"{tissue_rename}"] + 0.001)
-        df[f"{tissue_rename}_foldchange"] = df[f"{tissue_rename}"] / df["all_tissues"]
 
-        # add .001 to TPM to avoid negative infinity
-
-        df[f"{tissue_rename}_foldchange"] = df[f"{tissue_rename}_foldchange"] + 0.001
-
-    return df.apply(lambda x: np.log2(x))
+        # convert tpms to log2 (+0.25 to avoid negative infinity)
+        df[f"{tissue_rename}"] = np.log2(df[f"{tissue_rename}"] + 0.25)
+        df[f"{tissue_rename}_foldchange"] = df[f"{tissue_rename}"] - df["all_tissues"]
+    return df
 
 
 @time_decorator(print_args=False)
@@ -205,8 +245,8 @@ def _get_target_values_for_tissues(
     for tissue in tissue_params:
         all_dict[tissue] = _get_dict_with_target_array(
             split_dict=split,
-            tpmkey=tissue_params[tissue][0],
-            prokey=tissue_params[tissue][1],
+            tpmkey=tissue_params[tissue][1],
+            prokey=tissue_params[tissue][0],
             tpm_median_and_fold_change_df=tpm_median_and_fold_change_df,
             protein_median_and_fold_change_df=protein_median_and_fold_change_df,
         )
@@ -223,36 +263,46 @@ def tissue_targets_for_training(
     tissues: bool = True,
 ):
     """ """
-    # load expression values
+    # load tpm median across all tissues and samples
     with open(expression_median_across_all, "rb") as file:
         tpm_all_median = pickle.load(file)
 
-    # load protein values
-    protein_abundance_median_across_all = _protein_std_dev_and_mean(
+    # load tpm median matrix
+    tpm_median_df = parse(expression_median_matrix).data_df
+
+    # load protein abundance median matrix
+    protein_abundance_tissue_matrix = _get_protein_adundance_tissue_matrix(
         protein_abundance_medians=protein_abundance_medians,
         graph="tissue",
     )
-    protein_abundance_median_matrix = _protein_abundance_all_tissue_median(
-        protein_abundance_matrix=protein_abundance_matrix
+
+    # load protein abundance median across all tissues and samples
+    protein_abundance_median_across_all = (
+        _get_protein_adbunance_median_across_all_tissues_samples(
+            protein_abundance_matrix=protein_abundance_matrix
+        )
     )
 
     # create dataframes with target medians and fold change(median in tissue /
-    # median across all tissues)
-    protein_median_and_fold_change_df = _calculate_fold_change_from_medians(
-        median_across_tissues=protein_abundance_median_across_all,
-        median_matrix=protein_abundance_median_matrix,
-        type="protein",
+    # median across all tissues) for TPM
+    tpm_median_and_fold_change_df = _calculate_fold_change_from_medians(
+        median_matrix=tpm_median_df,
+        median_across_tissues=tpm_all_median,
+        type="tpm",
     )
 
-    tpm_median_df = parse(expression_median_matrix).data_df
-    tpm_median_and_fold_change_df = _calculate_fold_change_from_medians(
-        tpm_median_df, tpm_all_median, type="tpm"
+    # create dataframes with target medians and fold change(median in tissue /
+    # median across all tissues) for protein abundance
+    protein_median_and_fold_change_df = _calculate_fold_change_from_medians(
+        median_matrix=protein_abundance_tissue_matrix,
+        median_across_tissues=protein_abundance_median_across_all,
+        type="protein",
     )
 
     # parse targets into a dictionary for each gene/tissue combination
     targets = {
         data_split: _get_target_values_for_tissues(
-            tissue_paramas=tissue_params,
+            tissue_params=tissue_params,
             split=split[data_split],
             tpm_median_and_fold_change_df=tpm_median_and_fold_change_df,
             protein_median_and_fold_change_df=protein_median_and_fold_change_df,
