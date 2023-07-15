7abec5534c4385b55ca92a3a58040eea5d865901
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2023-07-14 23:22:23 -0400

    fix directory typo

--- genomic_graph_mutagenesis/local_context_parser.py
+++ genomic_graph_mutagenesis/local_context_parser.py
@@ -90,7 +90,7 @@ class LocalContextParser:
                 genes=genes,
                 gene_windows=gene_windows,
                 base_nodes=f"{self.tissue_dir}/local/basenodes_hg38.txt",
-                gct=f"{self.shared_dir}/{params['interaction']['gct']}",
+                gct=f"{self.root_dir}/shared_data/{params['interaction']['gct']}",
             )
 
         # prepare references
