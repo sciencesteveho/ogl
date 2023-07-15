210d0a4e4be7980c3acd798c6af48d0f18d5a8df
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2023-07-15 17:06:30 -0400

    add date to savestring

--- genomic_graph_mutagenesis/gnn.py
+++ genomic_graph_mutagenesis/gnn.py
@@ -495,36 +495,36 @@ def main() -> None:
         f"models/{savestr}/{savestr}_mse_{best_validation}.pt",
     )
 
-    # GNN Explainer!
-    if args.model != "MLP":
-        with open(
-            "/ocean/projects/bio210019p/stevesho/data/preprocess/graphs/explainer_node_ids.pkl",
-            "rb",
-        ) as file:
-            ids = pickle.load(file)
-        explain_path = "/ocean/projects/bio210019p/stevesho/data/preprocess/explainer"
-        explainer = Explainer(
-            model=model,
-            algorithm=GNNExplainer(epochs=200),
-            explanation_type="model",
-            node_mask_type="attributes",
-            edge_mask_type="object",
-            model_config=dict(mode="regression", task_level="node", return_type="raw"),
-        )
+    # # GNN Explainer!
+    # if args.model != "MLP":
+    #     with open(
+    #         "/ocean/projects/bio210019p/stevesho/data/preprocess/graphs/explainer_node_ids.pkl",
+    #         "rb",
+    #     ) as file:
+    #         ids = pickle.load(file)
+    #     explain_path = "/ocean/projects/bio210019p/stevesho/data/preprocess/explainer"
+    #     explainer = Explainer(
+    #         model=model,
+    #         algorithm=GNNExplainer(epochs=200),
+    #         explanation_type="model",
+    #         node_mask_type="attributes",
+    #         edge_mask_type="object",
+    #         model_config=dict(mode="regression", task_level="node", return_type="raw"),
+    #     )
 
-        data = data.to(device)
-        for index in random.sample(ids, 5):
-            explanation = explainer(data.x, data.edge_index, index=index)
+    #     data = data.to(device)
+    #     for index in random.sample(ids, 5):
+    #         explanation = explainer(data.x, data.edge_index, index=index)
 
-            print(f"Generated explanations in {explanation.available_explanations}")
+    #         print(f"Generated explanations in {explanation.available_explanations}")
 
-            path = f"{explain_path}/feature_importance_{savestr}_{best_validation}.png"
-            explanation.visualize_feature_importance(path, top_k=10)
-            print(f"Feature importance plot has been saved to '{path}'")
+    #         path = f"{explain_path}/feature_importance_{savestr}_{best_validation}.png"
+    #         explanation.visualize_feature_importance(path, top_k=10)
+    #         print(f"Feature importance plot has been saved to '{path}'")
 
-            path = f"{explain_path}/subgraph_{savestr}_{best_validation}.pdf"
-            explanation.visualize_graph(path)
-            print(f"Subgraph visualization plot has been saved to '{path}'")
+    #         path = f"{explain_path}/subgraph_{savestr}_{best_validation}.pdf"
+    #         explanation.visualize_graph(path)
+    #         print(f"Subgraph visualization plot has been saved to '{path}'")
 
 
 if __name__ == "__main__":
