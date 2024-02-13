423a87cb28d002e293fa934a6d4cee91abf559a2
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2024-02-12 23:42:17 -0500

    use asarray

--- omics_graph_learning/graph_constructor.py
+++ omics_graph_learning/graph_constructor.py
@@ -160,8 +160,8 @@ def _nx_to_tensors(
     """
     graph = nx.convert_node_labels_to_integers(graph, ordering="sorted")
     edges = np.array([[edge[0], edge[1]] for edge in nx.to_edgelist(graph)]).T
-    node_features = np.array(
-        [np.array(list(graph.nodes[node].values())) for node in sorted(graph.nodes)]
+    node_features = np.asarray(
+        [list(graph.nodes[node].values()) for node in sorted(graph.nodes)]
     )
     edge_features = [graph[u][v][2] for u, v in edges.T]
 
