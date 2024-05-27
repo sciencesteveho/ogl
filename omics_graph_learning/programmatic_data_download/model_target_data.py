5ae1bf6ff6526e9f13a6b8c1afaaeddf826ac679
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2024-05-27 16:31:39 -0400

    safety

--- omics_graph_learning/programmatic_data_download/model_target_data.py
+++ omics_graph_learning/programmatic_data_download/model_target_data.py
@@ -1,20 +1,55 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# // TO-DO //
-# - [ ] first TODO
-#   - [ ] nested TODO
 
-
-"""_summary_ of project"""
+"""Code to download and process GTEx gene expression data for training targets. Note that the protein expression data is available as XLSX files and thus are not included in the script."""
 
 import os
+import pathlib
 import pickle
 
+from cmapPy.pandasGEXpress.parse_gct import parse  # type: ignore
+import pandas as pd
 
-def place_holder_function() -> None:
-    """_summary_ of function"""
-    pass
+DOWNLOADS_URLS = [
+    "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct.gz",
+    "https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz",
+]
+
+
+def _check_and_download(file: str) -> None:
+    """Download a given file if the file does not already exist"""
+    if not os.path.exists(file):
+        print(f"Downloading {file}...")
+        os.system(
+            f"wget -O {file} https://s3.amazonaws.com/keras-datasets/mnist.pkl.gz"
+        )
+    else:
+        print(f"{file} already exists.")
+
+
+def _tpm_median_across_all_tissues(
+    median_across_all_file: pathlib.PosixPath,
+    all_matrix_gct: str,
+) -> None:
+    """Get the median TPM per gene across ALL samples within GTEx V8 GCT and
+    saves it. Because the file is large and requires a lot of memory, we ran
+    this separately from the produce_training_targets function and is only run once.
+
+    Args:
+        median_across_all_file (str): /path/to/median_across_all_file
+        all_matrix_gct (str): /path/to/gtex gct file
+    """
+    try:
+        if not median_across_all_file.exists():
+            median_series = pd.Series(
+                parse(all_matrix_gct).data_df.median(axis=1), name="all_tissues"
+            ).to_frame()
+            median_series.to_pickle(median_across_all_file, mode="xb")
+        else:
+            print("File already exists")
+    except FileExistsError:
+        print("File already exists!")
 
 
 def main() -> None:
