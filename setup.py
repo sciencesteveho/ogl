402dc320cd24441c64fb62258e911b34d0ad5b8b
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2024-09-08 20:33:43 -0400

    Fix: typo

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
-    name='omics_graph_learning',
-    version='development',
+    name="omics_graph_learning",
+    version="1.0.dev",
     packages=find_packages(),
 )
