8ad401e717d92148e0cb271ef3266141edad6a95
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2024-10-13 12:48:37 -0400

    Some more regularization

--- examples/slurm/submit.sh
+++ examples/slurm/submit.sh
@@ -25,8 +25,6 @@ conda activate /ocean/projects/bio210019p/stevesho/ogl
 
 
 
-
-
 configs=(all_celllines_alloopshicfdr.yaml)
 for config in "${configs[@]}"; do
   python ogl/omics_graph_learning/ogl_pipeline.py \
@@ -39,7 +37,7 @@ for config in "${configs[@]}"; do
     --activation gelu \
     --dimensions 200 \
     --batch_size 32 \
-    --learning_rate 0.0005 \
+    --learning_rate 0.00001 \
     --optimizer AdamW \
     --scheduler cosine \
     --dropout 0.3 \
@@ -60,11 +58,11 @@ for config in "${configs[@]}"; do
     --linear_layers 2 \
     --activation gelu \
     --dimensions 200 \
-    --batch_size 64 \
-    --learning_rate 0.0001 \
+    --batch_size 32 \
+    --learning_rate 0.00001 \
     --optimizer AdamW \
     --scheduler cosine \
-    --dropout 0.5 \
+    --dropout 0.3 \
     --residual distinct_source \
     --heads 2 \
     --positional_encoding
